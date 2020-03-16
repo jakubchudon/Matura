8298a972c1d3b4fe78f67788dcf9b852d0a5ab07
Jakub <jakubchudon@icloud.com>
2020-03-16 21:55:54 +0100

    v1

---
+++ 2017/2017_b.py
@@ -0,0 +1,8 @@
+plik=open("C:/Users/Jakub/PycharmProjects/Matura/Pliki/2017_maj/dane.txt")
+wynik=0
+for linia in plik:
+    linia=linia.strip()
+    linia=linia.split()
+    if linia!=(reversed(linia)):
+        wynik+=1
+print(wynik)
---
+++ 2017/2017_c.py
@@ -0,0 +1,41 @@
+plik=open("C:/Users/Jakub/PycharmProjects/Matura/Pliki/2017_maj/dane.txt")
+zapis=open("C:/Users/Jakub/PycharmProjects/Matura/Pliki/2017_maj/zapis.txt",'w')
+kontrastujace=0
+tablica=[]
+for linia in plik:
+    linia=linia.strip()
+    linia=linia.split()
+    rzad=[]
+    for x in linia:
+        rzad.append(int(x))
+    tablica.append(rzad)
+for y in range(len(tablica)):
+    for x in range(len(tablica[0])):
+        kontrast = False
+        #lewy
+        if x>0:
+            r=abs(tablica[y][x]-tablica[y][x-1])
+            if r>128:
+                kontrast=True
+        #prawy
+        if x<319:
+            r=abs(tablica[y][x]-tablica[y][x+1])
+            if r>128:
+                kontrast=True
+        #gora
+        if y>0:
+            r=abs(tablica[y][x]-tablica[y-1][x])
+            if r>128:
+                kontrast=True
+        #dol
+        if y<199:
+            r = abs(tablica[y][x] - tablica[y+1][x])
+            if r > 128:
+                kontrast = True
+        #sprawdzenie
+        if kontrast==True:
+            kontrastujace+=1
+print(kontrastujace)
+
+
+
